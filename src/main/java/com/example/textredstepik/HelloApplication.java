4e43cec8d2e2072f191a1cb3bd0cefa4cb416604
iakon <iakonien@gmail.com>
2022-10-01 18:50:25 +0300

    Простейший текстовый редактор (финальный коммит)

--- src/main/java/com/example/textredstepik/HelloApplication.java
+++ src/main/java/com/example/textredstepik/HelloApplication.java
@@ -8,11 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Stage ssstage;
     @Override
     public void start(Stage stage) throws IOException {
+        ssstage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Текстовый редактор");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/textredstepik/HelloController.java
+++ src/main/java/com/example/textredstepik/HelloController.java
@@ -2,13 +2,94 @@ package com.example.textredstepik;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.skin.TextInputControlSkin;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.util.Scanner;
+
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label name;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button open;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private Button saveAs;
+
+    @FXML
+    private TextArea textArea;
+    FileWriter fw;
+    FileReader fr;
+    File selectedFile;
+
+    public void initialize(){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(
+                "C:\\Users\\iakon\\IdeaProjects\\TextRedStepik\\textfiles"));
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(
+                "C:\\Users\\iakon\\IdeaProjects\\TextRedStepik\\textfiles"));
+
+
+        open.setOnAction(event -> {
+            selectedFile = fileChooser.showOpenDialog(HelloApplication.ssstage);
+            if(selectedFile != null){
+                name.setText(String.valueOf(selectedFile).substring(
+                        String.valueOf(selectedFile).lastIndexOf('\\') + 1
+                ));
+
+                try {
+                    fr = new FileReader(selectedFile);
+                    Scanner scan = new Scanner(fr);
+
+                    textArea.setText("");
+                    while (scan.hasNextLine()){
+                        textArea.appendText(scan.nextLine());
+                        if(scan.hasNextLine()) textArea.appendText("\n");
+                    }
+
+                    fr.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        save.setOnAction(event -> {
+            if (selectedFile != null){
+                try {
+                    fw = new FileWriter(selectedFile);
+                    fw.write(textArea.getText());
+                    fw.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        saveAs.setOnAction(event -> {
+            File file = fileChooser.showSaveDialog(HelloApplication.ssstage);
+            if(file != null){
+                try {
+                    fw = new FileWriter(file);
+                    fw.write(textArea.getText());
+                    fw.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
+
 }
